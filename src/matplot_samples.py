32248c96cc47fd6c3ffb20ee999c4b1d05852c12
Khalil <ksalha@hotmail.com>
2025-10-13 00:47:04 -0300

    Reduzindo função

--- src/matplot_samples.py
+++ src/matplot_samples.py
@@ -3,36 +3,22 @@ _sys.path.append('../')
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def plot_average_life_expectancy_by_continent(df):
-    life_by_cont = df.groupby(['year','continent'])['lifeExp'].mean().unstack()
+
+def plot_line_chart(df, operationType, groupByList, returnField, title = "Title", xLabel = "xLabel", yLabel = "yLabel"):
+
+    if operationType == "mean":
+        life_by_cont = df.groupby(groupByList)[returnField].mean().unstack()
+    
     plt.figure(figsize=(10,5))
+
     for cont in life_by_cont.columns:
         plt.plot(life_by_cont.index, life_by_cont[cont], label=cont)
-    plt.title('Média da Expectativa de Vida por Continente')
-    plt.xlabel('Ano')
-    plt.ylabel('Expectativa de Vida (anos)')
+
+    plt.title(title)
+    plt.xlabel(xLabel)
+    plt.ylabel(yLabel)
     plt.legend()
     plt.show()
 
-
-def plot_average_per_capita_income_by_continent(df):
-    life_by_cont = df.groupby(['year','continent'])['gdpPercap'].mean().unstack()
-    plt.figure(figsize=(10,5))
-    for cont in life_by_cont.columns:
-        plt.plot(life_by_cont.index, life_by_cont[cont], label=cont)
-    plt.title('Média do Renda Per Capita Por Continente')
-    plt.xlabel('Ano')
-    plt.ylabel('Renda Per Capita')
-    plt.legend()
-    plt.show()
-
-def plot_total_population_by_continent(df):
-    life_by_cont = df.groupby(['year','continent'])['pop'].mean().unstack()
-    plt.figure(figsize=(10,5))
-    for cont in life_by_cont.columns:
-        plt.plot(life_by_cont.index, life_by_cont[cont], label=cont)
-    plt.title('Média Populacional por Continente')
-    plt.xlabel('Ano')
-    plt.ylabel('População')
-    plt.legend()
-    plt.show()
+def plot_scatter_chart (df, operationType, groupByList, returnField, title = "Title", xLabel = "xLabel", yLabel = "yLabel"):
+    plt.show() #em desenvolvimento
