5a0767b97a8c0ea8fab358cbea310e0dbf667a7e
Khalil <ksalha@hotmail.com>
2025-10-15 21:04:02 -0300

    Incluindo Grafico PIB vs Expectativa de Vida Por Continente

--- src/matplot_samples.py
+++ src/matplot_samples.py
@@ -4,21 +4,52 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def plot_line_chart(df, operationType, groupByList, returnField, title = "Title", xLabel = "xLabel", yLabel = "yLabel"):
-
-    if operationType == "mean":
-        life_by_cont = df.groupby(groupByList)[returnField].mean().unstack()
-    
+def plot_line_chart(df, group, title = "Title", xLabel = "xLabel", yLabel = "yLabel"):
     plt.figure(figsize=(10,5))
-
-    for cont in life_by_cont.columns:
-        plt.plot(life_by_cont.index, life_by_cont[cont], label=cont)
-
+    for cont in group.columns:
+        plt.plot(group.index, group[cont], label=cont)
     plt.title(title)
     plt.xlabel(xLabel)
     plt.ylabel(yLabel)
     plt.legend()
     plt.show()
 
-def plot_scatter_chart (df, operationType, groupByList, returnField, title = "Title", xLabel = "xLabel", yLabel = "yLabel"):
-    plt.show() #em desenvolvimento
+def plot_scatter_chart (df, xData, yData, title = "Title", xLabel = "xLabel", yLabel = "yLabel", showGrid = True):
+    plt.figure(figsize=(10, 6))
+    plt.scatter(xData, yData, alpha=0.6)
+    plt.title(title)
+    plt.ylabel(yLabel)
+    plt.xlabel(xLabel)
+    plt.grid(showGrid)
+    plt.show()
+
+def scatter_subplots(df, 
+                     dfPrincipalFieldName,  
+                     dfXColFieldName,
+                     dfYColFieldName,
+                     nRows, 
+                     nCols, 
+                     dataToAnalyze, 
+                     x_colLabel='lifeExp', 
+                     y_colLabel='gdpPercap', 
+                     cores=None, 
+                     titulo_geral=None):
+    fig, axs = plt.subplots(nRows, nCols, figsize=(14,10), constrained_layout=True)
+    axs = axs.flatten()  # transformar em array 1D para fácil iteração
+
+    if cores is None:
+        cores = plt.cm.tab10.colors
+    
+    for i, value in enumerate(dataToAnalyze):
+        color = cores[i % len(cores)]
+        sub_df = df[df[dfPrincipalFieldName] == value]
+        axs[i].scatter(sub_df[dfXColFieldName], sub_df[dfYColFieldName], alpha=0.6, color=color)
+        axs[i].set_title(value)
+        axs[i].set_xlabel(x_colLabel)
+        axs[i].set_ylabel(y_colLabel)
+        axs[i].grid(True)
+
+    if titulo_geral:
+        fig.suptitle(titulo_geral, fontsize=16)
+    
+    plt.show()
